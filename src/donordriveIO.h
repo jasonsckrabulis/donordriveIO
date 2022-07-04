6b9ba6996759ea8befc4b05460a83825dccb96c4
Jason Sckrabulis <jason.sckrabulis@gmail.com>
2022-07-04 19:28:48 -0400

    Update donordriveIO.h

--- src/donordriveIO.h
+++ src/donordriveIO.h
@@ -1,1 +1,20 @@
+/*
+  donordriveIO.h - An Arduino library to enable access to the DonorDrive API (https://github.com/DonorDrive/PublicAPI)
+  Created by Jason P Sckrabulis
+*/
+#ifndef donordriveIO_h
+#define donordriveIO_h
 
+#include "Arduino.h"
+
+class donordriveIO
+{
+  public:
+    ddAPI(/*variables needed in the function*/);
+    //functions listed as void funcName();
+  private:
+    //private variables
+};
+
+#endif
+    
